5e5e600949661d8a46131255f98806fc1961c5ff
Susana Susana <sfigueroa3@uc.cl>
2023-07-11 12:34:32 -0400

    :sparkles: update load weights

--- load_weights.py
+++ load_weights.py
@@ -2,60 +2,60 @@ import gdown
 
 # Descarga de pesos de VGG16
 URL = "https://drive.google.com/file/d/1fHYY_mTzmawP-fhU2JYgh3tacw990kX7/view?usp=sharing"
-gdown.download(URL, "VGG16/weights/vgg16_015_1e-3_Adam.pt", quiet=False)
+gdown.download(URL, "VGG16/weights/vgg16_015_1e-3_Adam.pt", quiet=False, fuzzy=True)
 
 URL = "https://drive.google.com/file/d/1oovHz0Mi7ft-tVhDY_DCsteGOVMarPmt/view?usp=sharing"
-gdown.download(URL, "VGG16/weights/vgg16_030_5e-2_SGD.pt", quiet=False)
+gdown.download(URL, "VGG16/weights/vgg16_030_5e-2_SGD.pt", quiet=False, fuzzy=True)
 
 URL = "https://drive.google.com/file/d/1Tnj-5_i44v-u6BHtSjNbwpKpWn16Hp_l/view?usp=sharing"
-gdown.download(URL, "VGG16/weights/vgg16_080_5e-3_SGD.pt", quiet=False)
+gdown.download(URL, "VGG16/weights/vgg16_080_5e-3_SGD.pt", quiet=False, fuzzy=True)
 
 URL = "https://drive.google.com/file/d/1ZcfyKSZV0nQWNutUZZTHxLWruFB2Iw_z/view?usp=sharing"
-gdown.download(URL, "VGG16/weights/vgg16_099_1e-4_Adam.pt", quiet=False)
+gdown.download(URL, "VGG16/weights/vgg16_099_1e-4_Adam.pt", quiet=False, fuzzy=True)
 
 
 # Descarga de pesos de EffiecientNet
 URL = "https://drive.google.com/file/d/1Gh5C_qOvzKT0H3U8povlVbzTQ0eEPn54/view?usp=sharing"
-gdown.download(URL, "EfficientNet/weights/EfficientNet_009_2e-3_Adam.pt", quiet=False)
+gdown.download(URL, "EfficientNet/weights/EfficientNet_009_2e-3_Adam.pt", quiet=False, fuzzy=True)
 
 URL = "https://drive.google.com/file/d/14A1Hfj_F-srVqOPAKwOO5tJqTqMUU9du/view?usp=sharing"
-gdown.download(URL, "EfficientNet/weights/EfficientNet_050_1e-4_Adam.pt", quiet=False)
+gdown.download(URL, "EfficientNet/weights/EfficientNet_050_1e-4_Adam.pt", quiet=False, fuzzy=True)
 
 URL = "https://drive.google.com/file/d/1_uuqr4cWQdUubB6eqk-iDTTYASX-8v-K/view?usp=sharing"
-gdown.download(URL, "EfficientNet/weights/EfficientNet_099_1e-2_ASGD", quiet=False)
+gdown.download(URL, "EfficientNet/weights/EfficientNet_099_1e-2_ASGD", quiet=False, fuzzy=True)
 
 
 # Descarga de pesos de RegNet
 URL = "https://drive.google.com/file/d/1PoBcGRpvI5EwSBFRAaZCoGwxJ0rg6_H0/view?usp=sharing"
-gdown.download(URL, "RegNet/weights/RegNet_007_1e-2_SGD.pt", quiet=False)
+gdown.download(URL, "RegNet/weights/RegNet_007_1e-2_SGD.pt", quiet=False, fuzzy=True)
 
 URL = "https://drive.google.com/file/d/1eNxvNgJJZYxmryf0nFjxsqAqdYixhXNI/view?usp=sharing"
-gdown.download(URL, "RegNet/weights/RegNet_009_1e-1_SGD.pt", quiet=False)
+gdown.download(URL, "RegNet/weights/RegNet_009_1e-1_SGD.pt", quiet=False, fuzzy=True)
 
 
 # Descarga de pesos de ResNet
 URL = "https://drive.google.com/file/d/1i__A8ZuR0Ba9bGAjiyt_Y7AMSRbs77RX/view?usp=sharing"
-gdown.download(URL, "ResNet/weights/Resnet_049_1e-0_SGD.pt", quiet=False)
+gdown.download(URL, "ResNet/weights/Resnet_049_1e-0_SGD.pt", quiet=False, fuzzy=True)
 
 URL = "https://drive.google.com/file/d/1hzaGbuKmD4Skw0FWhqMJUfPa67XBjEdT/view?usp=sharing"
-gdown.download(URL, "ResNet/weights/Resnet_049_1e-1_SGD.pt", quiet=False)
+gdown.download(URL, "ResNet/weights/Resnet_049_1e-1_SGD.pt", quiet=False, fuzzy=True)
 
 URL = "https://drive.google.com/file/d/1OeRqzptEH4M7_t-CkRQHh0NozjNjjd9E/view?usp=sharing"
-gdown.download(URL, "ResNet/weights/Resnet_049_1e-2_SGD.pt", quiet=False)
+gdown.download(URL, "ResNet/weights/Resnet_049_1e-2_SGD.pt", quiet=False, fuzzy=True)
 
 URL = "https://drive.google.com/file/d/1HK9iDe_Nz0g4mbWG0vqkPZucOpLuGJ_m/view?usp=sharing"
-gdown.download(URL, "ResNet/weights/resnet50_049.pt", quiet=False)
+gdown.download(URL, "ResNet/weights/resnet50_049.pt", quiet=False, fuzzy=True)
 
 
 # Descarga de pesos de ResNeXt
 URL = "https://drive.google.com/file/d/1GCO84JbU4jO1VDCxLz6AI3xMX9NTDReF/view?usp=sharing"
-gdown.download(URL, "ResNeXt/weights/ResNeXt_009_1e-2_Adam.pt", quiet=False)
+gdown.download(URL, "ResNeXt/weights/ResNeXt_009_1e-2_Adam.pt", quiet=False, fuzzy=True)
 
 URL = "https://drive.google.com/file/d/1UeYmAHyMuDFTpRlHojmKxIEGpQREWLex/view?usp=sharing"
-gdown.download(URL, "ResNeXt/weights/ResNeXt_009_1e-3_Adam", quiet=False)
+gdown.download(URL, "ResNeXt/weights/ResNeXt_009_1e-3_Adam", quiet=False, fuzzy=True)
 
 URL = "https://drive.google.com/file/d/1sAJszPCHHRQytHKG4c1Lat0RCPFwOllR/view?usp=sharing"
-gdown.download(URL, "ResNeXt/weights/ResNeXt_009_5e-3_Adam.pt", quiet=False)
+gdown.download(URL, "ResNeXt/weights/ResNeXt_009_5e-3_Adam.pt", quiet=False, fuzzy=True)
 
 URL = "https://drive.google.com/file/d/1ym7cKt92mkB6Iw4zIZgnwUKT5rzmvVlS/view?usp=sharingg"
-gdown.download(URL, "ResNeXt/weights/ResNeXt_009_6e-3_Adam.pt", quiet=False)
+gdown.download(URL, "ResNeXt/weights/ResNeXt_009_6e-3_Adam.pt", quiet=False, fuzzy=True)
